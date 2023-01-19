0044777c296b39b58980be1559afc6778ceb0cb0
Tahlil <bsse0803@iit.du.ac.bd>
2023-01-19 15:35:05 +0600

    Import env variable for deployment

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,8 +1,11 @@
 import { ethers } from "hardhat";
 import { writeFile } from 'fs/promises';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 async function main() {
-  // const lockedAmount = ethers.utils.parseEther("1");
+  const lockedAmount = ethers.utils.parseEther("1");
 
   const NFT = await ethers.getContractFactory("NFT");
   // const greeting = await Greeting.deploy("Hello world", { value: lockedAmount });
@@ -13,6 +16,7 @@ async function main() {
   console.log("Greeting contract deployed to: ", nft.address);
   // write
   await writeFile('./data.json', JSON.stringify({nftAddress: nft.address}));
+  
 }
 
 // We recommend this pattern to be able to use async/await everywhere
