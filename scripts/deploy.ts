cedb9a0f928d2ffde69c0c2ae6f8d46e2f80e6d2
Tahlil <bsse0803@iit.du.ac.bd>
2023-01-19 17:16:08 +0600

    Add scripts for init and deploy all contracts in goerli

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,22 +1,89 @@
 import { ethers } from "hardhat";
-import { writeFile } from 'fs/promises';
-import dotenv from 'dotenv';
+import { writeFile } from "fs/promises";
+import { ContractTransaction } from "ethers";
+import dotenv from "dotenv";
+import {
+  Facade,
+  Facade__factory,
+  Marketplace,
+  Marketplace__factory,
+  NFT,
+  NFT__factory,
+} from "../typechain";
 
 dotenv.config();
 
 async function main() {
-  const lockedAmount = ethers.utils.parseEther("1");
+  let Marketplace: Marketplace__factory,
+    marketplace: Marketplace,
+    NFT: NFT__factory,
+    nft: NFT,
+    Facade: Facade__factory,
+    facade: Facade;
+  const {
+    NETWORK_ADDRESS,
+    PLATFORM_ADDRESS,
+    PLATFORM_PRIMARY_PERCENTAGE,
+    PLATFORM_SECONDARY_PERCENTAGE,
+    NETWORK_PRIMARY_PERCENTAGE,
+    NETWORK_SECONDARY_PERCENTAGE,
+    ARTIST_PRIMARY_PERCENTAGE,
+    ARTIST_SECONDARY_PERCENTAGE,
+  } = process.env;
 
-  const NFT = await ethers.getContractFactory("NFT");
-  // const greeting = await Greeting.deploy("Hello world", { value: lockedAmount });
-  const nft = await NFT.deploy();
+  const NFTcontract = await ethers.getContractFactory("NFT");
+  nft = await NFTcontract.deploy();
 
   await nft.deployed();
+  const nftAddress = nft.address;
 
-  console.log("Greeting contract deployed to: ", nft.address);
-  // write
-  await writeFile('./data.json', JSON.stringify({nftAddress: nft.address}));
-  
+  // Deploy Marketplace Contract
+  Marketplace = await ethers.getContractFactory("Marketplace");
+  marketplace = await Marketplace.deploy(
+    PLATFORM_ADDRESS + "",
+    NETWORK_ADDRESS + "",
+    Number(PLATFORM_PRIMARY_PERCENTAGE),
+    Number(PLATFORM_SECONDARY_PERCENTAGE),
+    Number(NETWORK_PRIMARY_PERCENTAGE),
+    Number(NETWORK_SECONDARY_PERCENTAGE),
+    Number(ARTIST_PRIMARY_PERCENTAGE),
+    Number(ARTIST_SECONDARY_PERCENTAGE)
+  );
+  await marketplace.deployed();
+  const marketPlaceAddress = marketplace.address;
+
+  // Deploy Facade Contract
+  Facade = await ethers.getContractFactory("Facade");
+  facade = await Facade.deploy(nftAddress, marketPlaceAddress);
+
+  await facade.deployed();
+  const facadeAddress = facade.address;
+
+  // Configure NFT Caller
+  const nftConfigureFacadeTx: ContractTransaction =
+    await nft.configureFacadeCaller(facadeAddress);
+  await nftConfigureFacadeTx.wait();
+
+  const marketplaceConfigureFacadeTx: ContractTransaction =
+    await marketplace.configureFacadeCaller(facadeAddress);
+  await marketplaceConfigureFacadeTx.wait();
+  let explorerLink = "https://goerli.etherscan.io/address";
+  let nftContractLink = explorerLink + nftAddress,
+    marketplaceAddressLink = explorerLink + marketPlaceAddress,
+    facadeContractLink = explorerLink + facadeAddress;
+
+  // write addresses of the deployed contracts
+  await writeFile(
+    "./data.json",
+    JSON.stringify({
+      nftAddress: nftAddress,
+      nftContractLink: nftContractLink,
+      marketplaceAddress: marketPlaceAddress,
+      marketplaceAddressLink: marketplaceAddressLink,
+      facadeAddress: facadeAddress,
+      facadeContractLink: facadeContractLink
+    })
+  );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
